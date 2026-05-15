7d4290527d367d9f3af82cbd366b7ac108125217
Yan <Yan>
2026-05-15 16:37:54 +0700

    fix: SWM plugin Java target 21->25 to match server class version

--- swm-plugin/build.gradle.kts
+++ swm-plugin/build.gradle.kts
@@ -17,10 +17,10 @@ dependencies {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-    options.release.set(21)
+    options.release.set(25)
 }
