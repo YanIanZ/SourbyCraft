1b1568073f7185f244042b36d0c62d7e9aa0a5b9
Yan <Yan>
2026-05-19 03:06:04 +0700

    fix: restore swm-plugin standalone jar

---
+++ swm-plugin/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `java-library`
+}
+
+group = "dev.iyanz.sourbycraft"
+version = "4.0.0"
+description = "SourbyCraft SlimeWorldManager Plugin"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly(fileTree("../sourbycraft-server/build/libs") { include("*-server-*-REL.jar") })
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
